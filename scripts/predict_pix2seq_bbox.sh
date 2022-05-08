e2691b569d8b98515e0622ae8165a4969aa076a3
thomas0809 <thomasq0809@gmail.com>
2022-05-08 14:11:23 -0400

    multiple reaction

--- scripts/predict_pix2seq_bbox.sh
+++ scripts/predict_pix2seq_bbox.sh
@@ -18,7 +18,7 @@ NCCL_P2P_DISABLE=1 python main.py \
     --data_path preprocess \
     --image_path preprocess/images \
     --save_path $SAVE_PATH \
-    --test_file all.json \
+    --test_file singlerxn.json \
     --formats bbox \
     --input_size 1333 \
     --pix2seq \
--- scripts/train_pix2seq.sh
+++ scripts/train_pix2seq.sh
@@ -8,7 +8,7 @@ ACCUM_STEP=2
 
 DATESTR=$(date +"%m-%d-%H-%M")
 PIX2SEQ_CKPT=./ckpts/checkpoint_e299_ap370.pth
-SAVE_PATH=output/pix2seq_reaction_recursive
+SAVE_PATH=output/pix2seq_reaction_rotate
 mkdir -p ${SAVE_PATH}
 
 set -x
@@ -33,5 +33,5 @@ NCCL_P2P_DISABLE=1 python main.py \
     --label_smoothing 0. \
     --batch_size $((BATCH_SIZE / NUM_GPUS_PER_NODE / ACCUM_STEP)) \
     --gradient_accumulation_steps ${ACCUM_STEP} \
-    --do_train --do_valid --do_test \
+    --do_valid --do_test \
     --gpus $NUM_GPUS_PER_NODE  #  2>&1  | tee $SAVE_PATH/log_${DATESTR}.txt
