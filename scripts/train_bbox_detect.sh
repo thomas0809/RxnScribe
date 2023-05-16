a0f677b8934fbeeccb72025fa1314d1722b6d456
Vincent K Fan <vincentf@rosetta3.csail.mit.edu>
2023-05-16 14:22:52 -0400

    bbox training script

---
+++ scripts/train_bbox_detect.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+NUM_NODES=1
+NUM_GPUS_PER_NODE=4
+
+BATCH_SIZE=32
+ACCUM_STEP=2
+
+PIX2SEQ_CKPT=./ckpts/checkpoint_e299_ap370.pth
+SAVE_PATH=output/bbox_detect
+
+set -x
+mkdir -p $SAVE_PATH
+NCCL_P2P_DISABLE=1 python main.py \
+    --data_path data/detect/splits/annotations \
+    --image_path data/detect/images \
+    --save_path $SAVE_PATH \
+    --train_file detect_train.json\
+    --valid_file detect_val.json\
+    --test_file detect_test.json\
+    --format bbox \
+    --input_size 1333 \
+    --pix2seq \
+    --pix2seq_ckpt ${PIX2SEQ_CKPT} \
+    --pred_eos \
+    --lr 4e-4 \
+    --epochs 600 --eval_per_epoch 30 \
+    --warmup 0.02 \
+    --label_smoothing 0. \
+    --batch_size $((BATCH_SIZE / NUM_GPUS_PER_NODE / ACCUM_STEP)) \
+    --gradient_accumulation_steps ${ACCUM_STEP} \
+    --do_test \
+    --gpus $NUM_GPUS_PER_NODE
